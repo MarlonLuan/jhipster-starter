8a4aa91c01fe2116d2e7108bc288d4c48ba76bae
MarlonLuan <MarlonLuan@users.noreply.github.com>
2021-03-21 18:40:11 +0000

    7.2.0

---
+++ app/client/src/main/webapp/app/entities/country/update/country-update.component.spec.ts
@@ -0,0 +1,148 @@
+jest.mock('@angular/router');
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpResponse } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormBuilder } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { of, Subject } from 'rxjs';
+
+import { CountryService } from '../service/country.service';
+import { ICountry, Country } from '../country.model';
+import { IRegion } from 'app/entities/region/region.model';
+import { RegionService } from 'app/entities/region/service/region.service';
+
+import { CountryUpdateComponent } from './country-update.component';
+
+describe('Component Tests', () => {
+  describe('Country Management Update Component', () => {
+    let comp: CountryUpdateComponent;
+    let fixture: ComponentFixture<CountryUpdateComponent>;
+    let activatedRoute: ActivatedRoute;
+    let countryService: CountryService;
+    let regionService: RegionService;
+
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [HttpClientTestingModule],
+        declarations: [CountryUpdateComponent],
+        providers: [FormBuilder, ActivatedRoute],
+      })
+        .overrideTemplate(CountryUpdateComponent, '')
+        .compileComponents();
+
+      fixture = TestBed.createComponent(CountryUpdateComponent);
+      activatedRoute = TestBed.inject(ActivatedRoute);
+      countryService = TestBed.inject(CountryService);
+      regionService = TestBed.inject(RegionService);
+
+      comp = fixture.componentInstance;
+    });
+
+    describe('ngOnInit', () => {
+      it('Should call region query and add missing value', () => {
+        const country: ICountry = { id: '1361f429-3817-4123-8ee3-fdf8943310b2' };
+        const region: IRegion = { id: 'c9a07aaf-b018-4454-9c14-72750ed4f9a6' };
+        country.region = region;
+
+        const regionCollection: IRegion[] = [{ id: '8dd721ae-9167-4dc2-9412-27f29f791f3e' }];
+        jest.spyOn(regionService, 'query').mockReturnValue(of(new HttpResponse({ body: regionCollection })));
+        const expectedCollection: IRegion[] = [region, ...regionCollection];
+        jest.spyOn(regionService, 'addRegionToCollectionIfMissing').mockReturnValue(expectedCollection);
+
+        activatedRoute.data = of({ country });
+        comp.ngOnInit();
+
+        expect(regionService.query).toHaveBeenCalled();
+        expect(regionService.addRegionToCollectionIfMissing).toHaveBeenCalledWith(regionCollection, region);
+        expect(comp.regionsCollection).toEqual(expectedCollection);
+      });
+
+      it('Should update editForm', () => {
+        const country: ICountry = { id: '1361f429-3817-4123-8ee3-fdf8943310b2' };
+        const region: IRegion = { id: 'ab35acef-89da-4285-9f5e-26037da67825' };
+        country.region = region;
+
+        activatedRoute.data = of({ country });
+        comp.ngOnInit();
+
+        expect(comp.editForm.value).toEqual(expect.objectContaining(country));
+        expect(comp.regionsCollection).toContain(region);
+      });
+    });
+
+    describe('save', () => {
+      it('Should call update service on save for existing entity', () => {
+        // GIVEN
+        const saveSubject = new Subject<HttpResponse<Country>>();
+        const country = { id: '9fec3727-3421-4967-b213-ba36557ca194' };
+        jest.spyOn(countryService, 'update').mockReturnValue(saveSubject);
+        jest.spyOn(comp, 'previousState');
+        activatedRoute.data = of({ country });
+        comp.ngOnInit();
+
+        // WHEN
+        comp.save();
+        expect(comp.isSaving).toEqual(true);
+        saveSubject.next(new HttpResponse({ body: country }));
+        saveSubject.complete();
+
+        // THEN
+        expect(comp.previousState).toHaveBeenCalled();
+        expect(countryService.update).toHaveBeenCalledWith(country);
+        expect(comp.isSaving).toEqual(false);
+      });
+
+      it('Should call create service on save for new entity', () => {
+        // GIVEN
+        const saveSubject = new Subject<HttpResponse<Country>>();
+        const country = new Country();
+        jest.spyOn(countryService, 'create').mockReturnValue(saveSubject);
+        jest.spyOn(comp, 'previousState');
+        activatedRoute.data = of({ country });
+        comp.ngOnInit();
+
+        // WHEN
+        comp.save();
+        expect(comp.isSaving).toEqual(true);
+        saveSubject.next(new HttpResponse({ body: country }));
+        saveSubject.complete();
+
+        // THEN
+        expect(countryService.create).toHaveBeenCalledWith(country);
+        expect(comp.isSaving).toEqual(false);
+        expect(comp.previousState).toHaveBeenCalled();
+      });
+
+      it('Should set isSaving to false on error', () => {
+        // GIVEN
+        const saveSubject = new Subject<HttpResponse<Country>>();
+        const country = { id: '9fec3727-3421-4967-b213-ba36557ca194' };
+        jest.spyOn(countryService, 'update').mockReturnValue(saveSubject);
+        jest.spyOn(comp, 'previousState');
+        activatedRoute.data = of({ country });
+        comp.ngOnInit();
+
+        // WHEN
+        comp.save();
+        expect(comp.isSaving).toEqual(true);
+        saveSubject.error('This is an error!');
+
+        // THEN
+        expect(countryService.update).toHaveBeenCalledWith(country);
+        expect(comp.isSaving).toEqual(false);
+        expect(comp.previousState).not.toHaveBeenCalled();
+      });
+    });
+
+    describe('Tracking relationships identifiers', () => {
+      describe('trackRegionById', () => {
+        it('Should return tracked Region primary key', () => {
+          const entity = { id: '9fec3727-3421-4967-b213-ba36557ca194' };
+          const trackResult = comp.trackRegionById(0, entity);
+          expect(trackResult).toEqual(entity.id);
+        });
+      });
+    });
+  });
+});
---
+++ app/client/src/main/webapp/app/entities/job/service/job.service.spec.ts
@@ -0,0 +1,195 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
+import { IJob, Job } from '../job.model';
+
+import { JobService } from './job.service';
+
+describe('Service Tests', () => {
+  describe('Job Service', () => {
+    let service: JobService;
+    let httpMock: HttpTestingController;
+    let elemDefault: IJob;
+    let expectedResult: IJob | IJob[] | boolean | null;
+
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [HttpClientTestingModule],
+      });
+      expectedResult = null;
+      service = TestBed.inject(JobService);
+      httpMock = TestBed.inject(HttpTestingController);
+
+      elemDefault = {
+        id: 'AAAAAAA',
+        jobTitle: 'AAAAAAA',
+        minSalary: 0,
+        maxSalary: 0,
+      };
+    });
+
+    describe('Service methods', () => {
+      it('should find an element', () => {
+        const returnedFromService = Object.assign({}, elemDefault);
+
+        service.find('9fec3727-3421-4967-b213-ba36557ca194').subscribe(resp => (expectedResult = resp.body));
+
+        const req = httpMock.expectOne({ method: 'GET' });
+        req.flush(returnedFromService);
+        expect(expectedResult).toMatchObject(elemDefault);
+      });
+
+      it('should create a Job', () => {
+        const returnedFromService = Object.assign(
+          {
+            id: 'ID',
+          },
+          elemDefault
+        );
+
+        const expected = Object.assign({}, returnedFromService);
+
+        service.create(new Job()).subscribe(resp => (expectedResult = resp.body));
+
+        const req = httpMock.expectOne({ method: 'POST' });
+        req.flush(returnedFromService);
+        expect(expectedResult).toMatchObject(expected);
+      });
+
+      it('should update a Job', () => {
+        const returnedFromService = Object.assign(
+          {
+            id: 'BBBBBB',
+            jobTitle: 'BBBBBB',
+            minSalary: 1,
+            maxSalary: 1,
+          },
+          elemDefault
+        );
+
+        const expected = Object.assign({}, returnedFromService);
+
+        service.update(expected).subscribe(resp => (expectedResult = resp.body));
+
+        const req = httpMock.expectOne({ method: 'PUT' });
+        req.flush(returnedFromService);
+        expect(expectedResult).toMatchObject(expected);
+      });
+
+      it('should partial update a Job', () => {
+        const patchObject = Object.assign(
+          {
+            jobTitle: 'BBBBBB',
+            maxSalary: 1,
+          },
+          new Job()
+        );
+
+        const returnedFromService = Object.assign(patchObject, elemDefault);
+
+        const expected = Object.assign({}, returnedFromService);
+
+        service.partialUpdate(patchObject).subscribe(resp => (expectedResult = resp.body));
+
+        const req = httpMock.expectOne({ method: 'PATCH' });
+        req.flush(returnedFromService);
+        expect(expectedResult).toMatchObject(expected);
+      });
+
+      it('should return a list of Job', () => {
+        const returnedFromService = Object.assign(
+          {
+            id: 'BBBBBB',
+            jobTitle: 'BBBBBB',
+            minSalary: 1,
+            maxSalary: 1,
+          },
+          elemDefault
+        );
+
+        const expected = Object.assign({}, returnedFromService);
+
+        service.query().subscribe(resp => (expectedResult = resp.body));
+
+        const req = httpMock.expectOne({ method: 'GET' });
+        req.flush([returnedFromService]);
+        httpMock.verify();
+        expect(expectedResult).toContainEqual(expected);
+      });
+
+      it('should delete a Job', () => {
+        service.delete('9fec3727-3421-4967-b213-ba36557ca194').subscribe(resp => (expectedResult = resp.ok));
+
+        const req = httpMock.expectOne({ method: 'DELETE' });
+        req.flush({ status: 200 });
+        expect(expectedResult);
+      });
+
+      describe('addJobToCollectionIfMissing', () => {
+        it('should add a Job to an empty array', () => {
+          const job: IJob = { id: '9fec3727-3421-4967-b213-ba36557ca194' };
+          expectedResult = service.addJobToCollectionIfMissing([], job);
+          expect(expectedResult).toHaveLength(1);
+          expect(expectedResult).toContain(job);
+        });
+
+        it('should not add a Job to an array that contains it', () => {
+          const job: IJob = { id: '9fec3727-3421-4967-b213-ba36557ca194' };
+          const jobCollection: IJob[] = [
+            {
+              ...job,
+            },
+            { id: '1361f429-3817-4123-8ee3-fdf8943310b2' },
+          ];
+          expectedResult = service.addJobToCollectionIfMissing(jobCollection, job);
+          expect(expectedResult).toHaveLength(2);
+        });
+
+        it("should add a Job to an array that doesn't contain it", () => {
+          const job: IJob = { id: '9fec3727-3421-4967-b213-ba36557ca194' };
+          const jobCollection: IJob[] = [{ id: '1361f429-3817-4123-8ee3-fdf8943310b2' }];
+          expectedResult = service.addJobToCollectionIfMissing(jobCollection, job);
+          expect(expectedResult).toHaveLength(2);
+          expect(expectedResult).toContain(job);
+        });
+
+        it('should add only unique Job to an array', () => {
+          const jobArray: IJob[] = [
+            { id: '9fec3727-3421-4967-b213-ba36557ca194' },
+            { id: '1361f429-3817-4123-8ee3-fdf8943310b2' },
+            { id: 'bd218adc-7090-4944-b3cd-6a2f7908d598' },
+          ];
+          const jobCollection: IJob[] = [{ id: '9fec3727-3421-4967-b213-ba36557ca194' }];
+          expectedResult = service.addJobToCollectionIfMissing(jobCollection, ...jobArray);
+          expect(expectedResult).toHaveLength(3);
+        });
+
+        it('should accept varargs', () => {
+          const job: IJob = { id: '9fec3727-3421-4967-b213-ba36557ca194' };
+          const job2: IJob = { id: '1361f429-3817-4123-8ee3-fdf8943310b2' };
+          expectedResult = service.addJobToCollectionIfMissing([], job, job2);
+          expect(expectedResult).toHaveLength(2);
+          expect(expectedResult).toContain(job);
+          expect(expectedResult).toContain(job2);
+        });
+
+        it('should accept null and undefined values', () => {
+          const job: IJob = { id: '9fec3727-3421-4967-b213-ba36557ca194' };
+          expectedResult = service.addJobToCollectionIfMissing([], null, job, undefined);
+          expect(expectedResult).toHaveLength(1);
+          expect(expectedResult).toContain(job);
+        });
+
+        it('should return initial array if no Job is added', () => {
+          const jobCollection: IJob[] = [{ id: '9fec3727-3421-4967-b213-ba36557ca194' }];
+          expectedResult = service.addJobToCollectionIfMissing(jobCollection, undefined, null);
+          expect(expectedResult).toEqual(jobCollection);
+        });
+      });
+    });
+
+    afterEach(() => {
+      httpMock.verify();
+    });
+  });
+});
