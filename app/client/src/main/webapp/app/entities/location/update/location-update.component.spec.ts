bd8b59c0f634dc69e500bc46e1cb1518115e95ef
Marlon Luan <marlonluan@gmail.com>
2022-02-25 18:51:14 -0300

    7.7.0

---
+++ app/client/src/main/webapp/app/entities/location/update/location-update.component.spec.ts
@@ -0,0 +1,153 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpResponse } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormBuilder } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of, Subject, from } from 'rxjs';
+
+import { LocationService } from '../service/location.service';
+import { ILocation, Location } from '../location.model';
+import { ICountry } from 'app/entities/country/country.model';
+import { CountryService } from 'app/entities/country/service/country.service';
+
+import { LocationUpdateComponent } from './location-update.component';
+
+describe('Location Management Update Component', () => {
+  let comp: LocationUpdateComponent;
+  let fixture: ComponentFixture<LocationUpdateComponent>;
+  let activatedRoute: ActivatedRoute;
+  let locationService: LocationService;
+  let countryService: CountryService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule.withRoutes([])],
+      declarations: [LocationUpdateComponent],
+      providers: [
+        FormBuilder,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: from([{}]),
+          },
+        },
+      ],
+    })
+      .overrideTemplate(LocationUpdateComponent, '')
+      .compileComponents();
+
+    fixture = TestBed.createComponent(LocationUpdateComponent);
+    activatedRoute = TestBed.inject(ActivatedRoute);
+    locationService = TestBed.inject(LocationService);
+    countryService = TestBed.inject(CountryService);
+
+    comp = fixture.componentInstance;
+  });
+
+  describe('ngOnInit', () => {
+    it('Should call country query and add missing value', () => {
+      const location: ILocation = { id: '1361f429-3817-4123-8ee3-fdf8943310b2' };
+      const country: ICountry = { id: '1dc3931a-f7f3-4e64-a0fc-036e2b1b4be0' };
+      location.country = country;
+
+      const countryCollection: ICountry[] = [{ id: '0623b3cc-a3d9-4adb-9023-2538b6114a5d' }];
+      jest.spyOn(countryService, 'query').mockReturnValue(of(new HttpResponse({ body: countryCollection })));
+      const expectedCollection: ICountry[] = [country, ...countryCollection];
+      jest.spyOn(countryService, 'addCountryToCollectionIfMissing').mockReturnValue(expectedCollection);
+
+      activatedRoute.data = of({ location });
+      comp.ngOnInit();
+
+      expect(countryService.query).toHaveBeenCalled();
+      expect(countryService.addCountryToCollectionIfMissing).toHaveBeenCalledWith(countryCollection, country);
+      expect(comp.countriesCollection).toEqual(expectedCollection);
+    });
+
+    it('Should update editForm', () => {
+      const location: ILocation = { id: '1361f429-3817-4123-8ee3-fdf8943310b2' };
+      const country: ICountry = { id: '3bfd7634-d887-49f0-87c0-6ab42e8f4414' };
+      location.country = country;
+
+      activatedRoute.data = of({ location });
+      comp.ngOnInit();
+
+      expect(comp.editForm.value).toEqual(expect.objectContaining(location));
+      expect(comp.countriesCollection).toContain(country);
+    });
+  });
+
+  describe('save', () => {
+    it('Should call update service on save for existing entity', () => {
+      // GIVEN
+      const saveSubject = new Subject<HttpResponse<Location>>();
+      const location = { id: '9fec3727-3421-4967-b213-ba36557ca194' };
+      jest.spyOn(locationService, 'update').mockReturnValue(saveSubject);
+      jest.spyOn(comp, 'previousState');
+      activatedRoute.data = of({ location });
+      comp.ngOnInit();
+
+      // WHEN
+      comp.save();
+      expect(comp.isSaving).toEqual(true);
+      saveSubject.next(new HttpResponse({ body: location }));
+      saveSubject.complete();
+
+      // THEN
+      expect(comp.previousState).toHaveBeenCalled();
+      expect(locationService.update).toHaveBeenCalledWith(location);
+      expect(comp.isSaving).toEqual(false);
+    });
+
+    it('Should call create service on save for new entity', () => {
+      // GIVEN
+      const saveSubject = new Subject<HttpResponse<Location>>();
+      const location = new Location();
+      jest.spyOn(locationService, 'create').mockReturnValue(saveSubject);
+      jest.spyOn(comp, 'previousState');
+      activatedRoute.data = of({ location });
+      comp.ngOnInit();
+
+      // WHEN
+      comp.save();
+      expect(comp.isSaving).toEqual(true);
+      saveSubject.next(new HttpResponse({ body: location }));
+      saveSubject.complete();
+
+      // THEN
+      expect(locationService.create).toHaveBeenCalledWith(location);
+      expect(comp.isSaving).toEqual(false);
+      expect(comp.previousState).toHaveBeenCalled();
+    });
+
+    it('Should set isSaving to false on error', () => {
+      // GIVEN
+      const saveSubject = new Subject<HttpResponse<Location>>();
+      const location = { id: '9fec3727-3421-4967-b213-ba36557ca194' };
+      jest.spyOn(locationService, 'update').mockReturnValue(saveSubject);
+      jest.spyOn(comp, 'previousState');
+      activatedRoute.data = of({ location });
+      comp.ngOnInit();
+
+      // WHEN
+      comp.save();
+      expect(comp.isSaving).toEqual(true);
+      saveSubject.error('This is an error!');
+
+      // THEN
+      expect(locationService.update).toHaveBeenCalledWith(location);
+      expect(comp.isSaving).toEqual(false);
+      expect(comp.previousState).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('Tracking relationships identifiers', () => {
+    describe('trackCountryById', () => {
+      it('Should return tracked Country primary key', () => {
+        const entity = { id: '9fec3727-3421-4967-b213-ba36557ca194' };
+        const trackResult = comp.trackCountryById(0, entity);
+        expect(trackResult).toEqual(entity.id);
+      });
+    });
+  });
+});
---
+++ app/server/src/main/java/com/mycompany/myapp/service/impl/TaskServiceImpl.java
@@ -0,0 +1,77 @@
+package com.mycompany.myapp.service.impl;
+
+import com.mycompany.myapp.domain.Task;
+import com.mycompany.myapp.repository.TaskRepository;
+import com.mycompany.myapp.service.TaskService;
+import com.mycompany.myapp.service.dto.TaskDTO;
+import com.mycompany.myapp.service.mapper.TaskMapper;
+import java.util.Optional;
+import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Service Implementation for managing {@link Task}.
+ */
+@Service
+@Transactional
+public class TaskServiceImpl implements TaskService {
+
+    private final Logger log = LoggerFactory.getLogger(TaskServiceImpl.class);
+
+    private final TaskRepository taskRepository;
+
+    private final TaskMapper taskMapper;
+
+    public TaskServiceImpl(TaskRepository taskRepository, TaskMapper taskMapper) {
+        this.taskRepository = taskRepository;
+        this.taskMapper = taskMapper;
+    }
+
+    @Override
+    public TaskDTO save(TaskDTO taskDTO) {
+        log.debug("Request to save Task : {}", taskDTO);
+        Task task = taskMapper.toEntity(taskDTO);
+        task = taskRepository.save(task);
+        return taskMapper.toDto(task);
+    }
+
+    @Override
+    public Optional<TaskDTO> partialUpdate(TaskDTO taskDTO) {
+        log.debug("Request to partially update Task : {}", taskDTO);
+
+        return taskRepository
+            .findById(taskDTO.getId())
+            .map(existingTask -> {
+                taskMapper.partialUpdate(existingTask, taskDTO);
+
+                return existingTask;
+            })
+            .map(taskRepository::save)
+            .map(taskMapper::toDto);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<TaskDTO> findAll(Pageable pageable) {
+        log.debug("Request to get all Tasks");
+        return taskRepository.findAll(pageable).map(taskMapper::toDto);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<TaskDTO> findOne(UUID id) {
+        log.debug("Request to get Task : {}", id);
+        return taskRepository.findById(id).map(taskMapper::toDto);
+    }
+
+    @Override
+    public void delete(UUID id) {
+        log.debug("Request to delete Task : {}", id);
+        taskRepository.deleteById(id);
+    }
+}
