248e44bf744a4082b9556dd5c3ac4f37969d1fd7
Marlon Luan <marlonluan@gmail.com>
2023-01-09 14:10:48 -0300

    7.9.3

---
+++ app/client/src/main/webapp/app/entities/job-history/job-history.test-samples.ts
@@ -0,0 +1,31 @@
+import dayjs from 'dayjs/esm';
+
+import { Language } from 'app/entities/enumerations/language.model';
+
+import { IJobHistory, NewJobHistory } from './job-history.model';
+
+export const sampleWithRequiredData: IJobHistory = {
+  id: 'f5e3cfb1-cf3d-4bcf-90ee-f9d5ac3f0fbf',
+};
+
+export const sampleWithPartialData: IJobHistory = {
+  id: '36427201-76e4-4885-a171-5926c59a55f5',
+  startDate: dayjs('2023-05-06T13:59'),
+  endDate: dayjs('2023-05-05T23:26'),
+};
+
+export const sampleWithFullData: IJobHistory = {
+  id: '641c82ec-f1c8-4416-9eab-abdf71b06628',
+  startDate: dayjs('2023-05-05T14:52'),
+  endDate: dayjs('2023-05-05T22:44'),
+  language: Language['ENGLISH'],
+};
+
+export const sampleWithNewData: NewJobHistory = {
+  id: null,
+};
+
+Object.freeze(sampleWithNewData);
+Object.freeze(sampleWithRequiredData);
+Object.freeze(sampleWithPartialData);
+Object.freeze(sampleWithFullData);
