bd44d58a3ec63778ef3604985f77f3989870a8c9
MarlonLuan <MarlonLuan@users.noreply.github.com>
2023-05-07 00:02:35 +0000

    latest

--- app/client/src/main/webapp/app/entities/employee/employee.test-samples.ts
+++ app/client/src/main/webapp/app/entities/employee/employee.test-samples.ts
@@ -18,7 +18,7 @@ export const sampleWithFullData: IEmployee = {
   lastName: 'Miller',
   email: 'Terrell.Pouros86@yahoo.com',
   phoneNumber: 'metrics Loan Brand',
-  hireDate: dayjs('2023-05-06T02:46'),
+  hireDate: dayjs('2023-05-06T12:34'),
   salary: 14616,
   commissionPct: 17603,
 };
