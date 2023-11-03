c5175efc8296c251775b05f5e60264675cfb4200
MarlonLuan <MarlonLuan@users.noreply.github.com>
2023-11-03 16:00:33 +0000

    beta

--- app/client/src/main/webapp/app/entities/job/detail/job-detail.component.ts
+++ app/client/src/main/webapp/app/entities/job/detail/job-detail.component.ts
@@ -1,22 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
+import SharedModule from 'app/shared/shared.module';
+import { DurationPipe, FormatMediumDatetimePipe, FormatMediumDatePipe } from 'app/shared/date';
 import { IJob } from '../job.model';
 
 @Component({
+  standalone: true,
   selector: 'jhi-job-detail',
   templateUrl: './job-detail.component.html',
+  imports: [SharedModule, RouterModule, DurationPipe, FormatMediumDatetimePipe, FormatMediumDatePipe],
 })
-export class JobDetailComponent implements OnInit {
-  job: IJob | null = null;
+export class JobDetailComponent {
+  @Input() job: IJob | null = null;
 
   constructor(protected activatedRoute: ActivatedRoute) {}
-
-  ngOnInit(): void {
-    this.activatedRoute.data.subscribe(({ job }) => {
-      this.job = job;
-    });
-  }
 
   previousState(): void {
     window.history.back();
