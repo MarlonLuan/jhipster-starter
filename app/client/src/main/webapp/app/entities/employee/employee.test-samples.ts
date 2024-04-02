e829de365b14d7c799f5526c79d8516f01da0fc7
MarlonLuan <MarlonLuan@users.noreply.github.com>
2024-04-02 11:20:39 +0000

    latest

--- app/client/src/main/webapp/app/entities/employee/employee.test-samples.ts
+++ app/client/src/main/webapp/app/entities/employee/employee.test-samples.ts
@@ -18,7 +18,7 @@ export const sampleWithFullData: IEmployee = {
   lastName: 'Gutkowski',
   email: 'Jeramy_Hickle-Crona@yahoo.com',
   phoneNumber: 'yuck with',
-  hireDate: dayjs('2024-04-02T02:22'),
+  hireDate: dayjs('2024-04-02T02:29'),
   salary: 20613,
   commissionPct: 22976,
 };
