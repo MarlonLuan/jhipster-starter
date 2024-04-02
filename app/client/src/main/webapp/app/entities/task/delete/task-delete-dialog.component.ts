352b7286458bfdc9338129ea1192e5316544665f
MarlonLuan <MarlonLuan@users.noreply.github.com>
2024-04-02 11:13:11 +0000

    latest

--- app/client/src/main/webapp/app/entities/task/delete/task-delete-dialog.component.ts
+++ app/client/src/main/webapp/app/entities/task/delete/task-delete-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
@@ -15,10 +15,8 @@ import { TaskService } from '../service/task.service';
 export class TaskDeleteDialogComponent {
   task?: ITask;
 
-  constructor(
-    protected taskService: TaskService,
-    protected activeModal: NgbActiveModal,
-  ) {}
+  protected taskService = inject(TaskService);
+  protected activeModal = inject(NgbActiveModal);
 
   cancel(): void {
     this.activeModal.dismiss();
