09c3509f277ff85061e138a98ebdc5ff048571c9
MarlonLuan <9463224+MarlonLuan@users.noreply.github.com>
2024-04-06 00:00:46 +0000

    next

--- app/client/src/main/webapp/app/entities/department/detail/department-detail.component.ts
+++ app/client/src/main/webapp/app/entities/department/detail/department-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import SharedModule from 'app/shared/shared.module';
@@ -12,7 +12,7 @@ import { IDepartment } from '../department.model';
   imports: [SharedModule, RouterModule, DurationPipe, FormatMediumDatetimePipe, FormatMediumDatePipe],
 })
 export class DepartmentDetailComponent {
-  @Input() department: IDepartment | null = null;
+  department = input<IDepartment | null>(null);
 
   previousState(): void {
     window.history.back();
--- app/client/src/main/webapp/app/entities/job/detail/job-detail.component.spec.ts
+++ app/client/src/main/webapp/app/entities/job/detail/job-detail.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { RouterTestingHarness, RouterTestingModule } from '@angular/router/testing';
+import { RouterTestingHarness } from '@angular/router/testing';
 import { of } from 'rxjs';
 
 import { JobDetailComponent } from './job-detail.component';
@@ -11,7 +11,7 @@ describe('Job Management Detail Component', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [JobDetailComponent, RouterTestingModule.withRoutes([], { bindToComponentInputs: true })],
+      imports: [JobDetailComponent],
       providers: [
         provideRouter(
           [
@@ -40,7 +40,7 @@ describe('Job Management Detail Component', () => {
       const instance = await harness.navigateByUrl('/', JobDetailComponent);
 
       // THEN
-      expect(instance.job).toEqual(expect.objectContaining({ id: '9fec3727-3421-4967-b213-ba36557ca194' }));
+      expect(instance.job()).toEqual(expect.objectContaining({ id: '9fec3727-3421-4967-b213-ba36557ca194' }));
     });
   });
 
