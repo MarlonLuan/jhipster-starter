c0a82129f574bab0050e4ec5da121add4b4c98b5
MarlonLuan <9463224+MarlonLuan@users.noreply.github.com>
2024-04-10 13:17:27 +0000

    beta

--- app/client/src/main/webapp/app/entities/department/detail/department-detail.component.spec.ts
+++ app/client/src/main/webapp/app/entities/department/detail/department-detail.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { RouterTestingHarness, RouterTestingModule } from '@angular/router/testing';
+import { RouterTestingHarness } from '@angular/router/testing';
 import { of } from 'rxjs';
 
 import { DepartmentDetailComponent } from './department-detail.component';
@@ -11,7 +11,7 @@ describe('Department Management Detail Component', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DepartmentDetailComponent, RouterTestingModule.withRoutes([], { bindToComponentInputs: true })],
+      imports: [DepartmentDetailComponent],
       providers: [
         provideRouter(
           [
@@ -40,7 +40,7 @@ describe('Department Management Detail Component', () => {
       const instance = await harness.navigateByUrl('/', DepartmentDetailComponent);
 
       // THEN
-      expect(instance.department).toEqual(expect.objectContaining({ id: '9fec3727-3421-4967-b213-ba36557ca194' }));
+      expect(instance.department()).toEqual(expect.objectContaining({ id: '9fec3727-3421-4967-b213-ba36557ca194' }));
     });
   });
 
--- app/client/src/main/webapp/app/entities/task/detail/task-detail.component.ts
+++ app/client/src/main/webapp/app/entities/task/detail/task-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import SharedModule from 'app/shared/shared.module';
@@ -12,7 +12,7 @@ import { ITask } from '../task.model';
   imports: [SharedModule, RouterModule, DurationPipe, FormatMediumDatetimePipe, FormatMediumDatePipe],
 })
 export class TaskDetailComponent {
-  @Input() task: ITask | null = null;
+  task = input<ITask | null>(null);
 
   previousState(): void {
     window.history.back();
