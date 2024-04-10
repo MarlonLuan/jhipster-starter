ffa27f8b2b583d04d2496605ddfe2facb40af452
MarlonLuan <9463224+MarlonLuan@users.noreply.github.com>
2024-04-10 13:08:32 +0000

    next

--- app/client/src/main/webapp/app/entities/location/detail/location-detail.component.ts
+++ app/client/src/main/webapp/app/entities/location/detail/location-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import SharedModule from 'app/shared/shared.module';
@@ -12,7 +12,7 @@ import { ILocation } from '../location.model';
   imports: [SharedModule, RouterModule, DurationPipe, FormatMediumDatetimePipe, FormatMediumDatePipe],
 })
 export class LocationDetailComponent {
-  @Input() location: ILocation | null = null;
+  location = input<ILocation | null>(null);
 
   previousState(): void {
     window.history.back();
