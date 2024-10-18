943c68b9a40abcca1eb0979aa58829105bb5f640
MarlonLuan <9463224+MarlonLuan@users.noreply.github.com>
2024-10-18 00:00:00 +0000

    next

--- my-workspace/client/src/main/webapp/app/entities/employee/service/employee.service.ts
+++ my-workspace/client/src/main/webapp/app/entities/employee/service/employee.service.ts
@@ -26,8 +26,8 @@ export type EntityArrayResponseType = HttpResponse<IEmployee[]>;
 
 @Injectable({ providedIn: 'root' })
 export class EmployeeService {
-  protected http = inject(HttpClient);
-  protected applicationConfigService = inject(ApplicationConfigService);
+  protected readonly http = inject(HttpClient);
+  protected readonly applicationConfigService = inject(ApplicationConfigService);
 
   protected resourceUrl = this.applicationConfigService.getEndpointFor('api/employees');
 
