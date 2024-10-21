e539a593c50e43d67542e2859a21b3f8e70c305d
MarlonLuan <9463224+MarlonLuan@users.noreply.github.com>
2024-10-21 00:00:00 +0000

    next

--- my-workspace/client/src/main/webapp/app/entities/location/list/location.component.ts
+++ my-workspace/client/src/main/webapp/app/entities/location/list/location.component.ts
@@ -43,10 +43,10 @@ export class LocationComponent implements OnInit {
   totalItems = 0;
   page = 1;
 
-  public router = inject(Router);
-  protected locationService = inject(LocationService);
-  protected activatedRoute = inject(ActivatedRoute);
-  protected sortService = inject(SortService);
+  public readonly router = inject(Router);
+  protected readonly locationService = inject(LocationService);
+  protected readonly activatedRoute = inject(ActivatedRoute);
+  protected readonly sortService = inject(SortService);
   protected modalService = inject(NgbModal);
   protected ngZone = inject(NgZone);
 
