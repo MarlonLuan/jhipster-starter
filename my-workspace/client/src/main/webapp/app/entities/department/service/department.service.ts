755ab34620921242ce608d94e6cd749ee2853ca0
MarlonLuan <9463224+MarlonLuan@users.noreply.github.com>
2024-10-21 00:00:00 +0000

    next

--- my-workspace/client/src/main/webapp/app/entities/department/service/department.service.ts
+++ my-workspace/client/src/main/webapp/app/entities/department/service/department.service.ts
@@ -14,8 +14,8 @@ export type EntityArrayResponseType = HttpResponse<IDepartment[]>;
 
 @Injectable({ providedIn: 'root' })
 export class DepartmentService {
-  protected http = inject(HttpClient);
-  protected applicationConfigService = inject(ApplicationConfigService);
+  protected readonly http = inject(HttpClient);
+  protected readonly applicationConfigService = inject(ApplicationConfigService);
 
   protected resourceUrl = this.applicationConfigService.getEndpointFor('api/departments');
 
--- my-workspace/client/src/main/webapp/app/entities/region/service/region.service.ts
+++ my-workspace/client/src/main/webapp/app/entities/region/service/region.service.ts
@@ -14,8 +14,8 @@ export type EntityArrayResponseType = HttpResponse<IRegion[]>;
 
 @Injectable({ providedIn: 'root' })
 export class RegionService {
-  protected http = inject(HttpClient);
-  protected applicationConfigService = inject(ApplicationConfigService);
+  protected readonly http = inject(HttpClient);
+  protected readonly applicationConfigService = inject(ApplicationConfigService);
 
   protected resourceUrl = this.applicationConfigService.getEndpointFor('api/regions');
 
